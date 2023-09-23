b5348224be7e812d6c3366cc553f5d9440abf631
unknown <desarrollo@creandoweb.net>
2023-09-22 22:51:47 -0500

    Se reconstruye el módulo de importación de cursos y usuarios:cursos. Se optimiza de tal forma que el front envia el archivo completo al back, y espera respuesta para confirmar al usuario https://trello.com/c/uI1Kra37

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -13,6 +13,12 @@ export default class UserService {
         });
     }
 
+    importExcel (data) {
+        return axios.post(`${process.env.VUE_APP_RUTA_API}/courses/userExcelImport`, data).then(res => {
+            return res.data;
+        });
+    }
+
     importLdapUsers () {
         return axios.get(`${process.env.VUE_APP_RUTA_API}/ldap/importusers`).then(res => {
             console.log('IMPORT USER', res.data);
